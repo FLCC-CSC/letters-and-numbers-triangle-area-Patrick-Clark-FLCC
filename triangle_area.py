38b390ce7fd0f8cbd5dc42df0b5d9683e763d8ee
Patrick-Clark-FLCC <pclark20@fingerlakes.edu>
2025-02-23 22:14:08 -0500

    Update triangle_area.py

--- triangle_area.py
+++ triangle_area.py
@@ -17,9 +17,9 @@
 
 ########## ENTER YER CODE BELOW THIS LINE ##########
 
-height_triangle = int(input('Enter the height: '))
-base_triangle = int(input('Enter the base: '))   
-area_triangle = (height_triangle * base_triangle)/2;
+height_triangle = int(input('Enter the height:\n'))
+base_triangle = int(input('Enter the base:\n'))   
+area_triangle = (height_triangle * base_triangle)/2
 
 print(f'The area of the triangle is {area_triangle}')  
 
